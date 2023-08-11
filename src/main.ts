cd2259cefb46acdb48b045d1be5a2856443ca32c
Alexey Yanchev <yanchevalexey@yandex.ru>
2023-08-11 23:01:04 +0300

    сделал общую обработку ошибок

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import "reflect-metadata";
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import { AllExceptionsFilter } from "./exceptions/all-exceptions.filter";
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const httpAdapter = app.get(HttpAdapterHost);
 
   app.use(helmet(
     // { crossOriginResourcePolicy: { policy: "cross-origin" } }
   ));
+
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
